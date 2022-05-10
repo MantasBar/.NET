800aebf3dc80ccc90e74bfbf0a462c608d5c7eb2
Mantas Baranauskas <mantas.baranauskas@hotmail.com>
2022-05-10 19:20:32 +0300

    start

---
+++ WarehouseManagement/WarehouseManagement.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using WarehouseManagement.ConsoleApp.Services;
+
+namespace WarehouseManagement.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var applicationService = new ApplicationService();
+
+            Console.WriteLine("Enter your command:");
+            var command = Console.ReadLine();
+
+            applicationService.Process(command);
+        }
+    }
+}
---
+++ WarehouseManagement/WarehouseManagement.ConsoleApp/Services/ApplicationService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagement.ConsoleApp.Services
+{
+    public class ApplicationService
+    {
+        private WarehouseService _warehouseService;
+        public ApplicationService()
+        {
+            _warehouseService = new WarehouseService();
+        }
+        public void Process(string command)
+        {
+            if (command.StartsWith("Add"))
+            {
+                _warehouseService.Add();
+            }
+            if (command.StartsWith("Remove"))
+            {
+                _warehouseService.Remove();
+            }
+            if (command.StartsWith("List"))
+            {
+                
+            }
+            if (command.StartsWith("Exit"))
+            {
+                return;
+            }
+            Console.WriteLine("Incorrect command");
+            // Interpret if command is valid
+            // Parse the command type and information
+            // Call appropriate WarehouseService command
+        }
+    }
+}
